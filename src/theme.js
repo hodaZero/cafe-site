ceb2250bf5065cfb2a9f1e0637dd458e17a9f805
ShimaaMohammedSoudy <shimaamohammd655@gmail.com>
2025-11-08 23:58:55 +0200

    first comit

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#33271eff",     
+  secondary: "#696055ff",  
+  bg: "#F8F5F2",          
+
+  orderProcessingBg: "#FEF3C7",
+  orderProcessingText: "#B45309",
+  orderCompletedBg: "#D1FAE5",
+  orderCompletedText: "#065F46",
+  orderCancelledBg: "#FEE2E2",
+  orderCancelledText: "#B91C1C",
+};
