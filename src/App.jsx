91db3db1b69e4d4ec8b3d1e051e6ace560364f98
ShimaaMohammedSoudy <shimaamohammd655@gmail.com>
2025-11-08 23:58:55 +0200

    first comit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import ProfilePage from "./pages/ProfilePage"; // استدعي البروفايل هنا
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<ProfilePage />} /> {/* خلى الصفحة الرئيسية بروفايل */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
