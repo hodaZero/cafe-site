15907f5816edf0129c09c7b09d3113ea9da6c0b8
ShimaaMohammedSoudy <shimaamohammd655@gmail.com>
2025-11-09 00:10:01 +0200

    Add profile page and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
-import ProfilePage from "./pages/ProfilePage"; // استدعي البروفايل هنا
+import ProfilePage from "./pages/ProfilePage"; 
+import CartPage from "./pages/Cart";
+import CheckoutPage from "./pages/Checkout";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<ProfilePage />} /> {/* خلى الصفحة الرئيسية بروفايل */}
+        <Route path="/" element={<ProfilePage />} /> 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
   );
