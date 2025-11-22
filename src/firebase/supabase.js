250bc91d241e795147d73fccf7a9991231d4fca4
Alshaimaa <alshaimaaabdelnaeem@gmail.com>
2025-11-22 07:11:34 +0200

    Add Supabase integration and image upload functionality
    - Updated package.json and package-lock.json to include @supabase/supabase-js dependency.
    - Implemented image upload feature in ProductForm and AdminProfile components.
    - Enhanced ProfilePage with image upload and user profile update capabilities.
    - Created storage services for handling image uploads to Supabase.
    - Added user management functions in usersServices.js for Firestore integration.

--- src/firebase/supabase.js
+++ src/firebase/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://gssxjbznbwsgssntpjdh.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdzc3hqYnpuYndzZ3NzbnRwamRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4MDA4MTcsImV4cCI6MjA3MzM3NjgxN30.n0nVke4qTrcd5U9Dw-pKTtT1WDeyDTL493GPdvXN0Uw'
+const supabaseUrl = 'https://cfcqerxevbgrrajzucfm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmY3FlcnhldmJncnJhanp1Y2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1ODk4NDksImV4cCI6MjA3OTE2NTg0OX0.fZZw7P30Bk54Mr8btXrRkkFQaWBWTeY2SHWT8FxeDBU'
 export const supabase = createClient(supabaseUrl, supabaseKey)
